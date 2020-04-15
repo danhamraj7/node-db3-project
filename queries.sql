3061d32c9991bed6e7a330d6025e5d82ae08b6e0
danhamraj <danhamraj@gmail.com>
2020-04-15 15:40:27 -0400

    bootstrap commit updated pkg,json and ran queries against the northwind database

--- queries.sql
+++ queries.sql
@@ -1,9 +1,42 @@
 -- Multi-Table Query Practice
 
--- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+-- Display the ProductName and CategoryName for all products 
+--in the database. Shows 77 records.
 
--- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+-- SELECT productname,
+-- categoryname
+-- FROM Category
+-- JOIN
+-- Product ON Product.CategoryId = Category.Id;
 
--- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+-- Display the order Id and shipper CompanyName for all orders 
+--placed before August 9 2012. Shows 429 records.
 
--- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+--SELECT [Order].Id, Shipper.CompanyName
+--FROM [Order]
+--JOIN Shipper
+--ON [Order].ShipVia = Shipper.Id
+--WHERE [Order].OrderDate < '2012-08-09'
+
+-- Display the name and quantity of the products ordered in order 
+--with Id 10251. Sort by ProductName. Shows 3 records.
+
+--SELECT Product.ProductName, OrderDetail.Quantity
+--FROM OrderDetail
+--JOIN Product
+--ON OrderDetail.ProductId = Product.Id
+--WHERE OrderDetail.OrderId = '10251'
+--ORDER BY Product.ProductName;
+
+-- Display the OrderID, Customer's Company Name and the 
+--employee's LastName for every order. All columns should be 
+--labeled clearly. Displays 16,789 records.
+
+--SELECT [Order].Id, Customer.CompanyName, Employee.LastName
+--FROM [Order]
+--JOIN [Customer], Employee
+--WHERE [Order].CustomerId = Customer.Id 
+--AND [Order].EmployeeId = Employee.Id;
+
+
+
